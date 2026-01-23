b7508a3e662cf005a384fd046fbc0a6e955c1766
David Quezada <david.quezada.m@gmail.com>
2026-01-22 21:40:38 -0300

    clase_1

---
+++ clase_1/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import consolasRouter from './routes/consolas.routes.js';
+import conectarBD from './config/database.js';
+
+const app = express();
+app.use(express.json());
+
+await conectarBD();
+
+app.use('/api/consolas', consolasRouter);
+
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Escuchando en el puerto ${PORT}`);
+});
